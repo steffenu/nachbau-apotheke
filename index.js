cb1fbc5d5c1b61815e84bb5495019290839d0c68
steffenu <steffen.development@gmail.com>
2020-01-20 16:05:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("init");
+
+document.getElementById("listener").addEventListener("click", function() {
+  console.log("nice");
+  myFunction();
+});
+
+function myFunction() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function() {
+    x.className = x.className.replace("show", "");
+  }, 3000);
+}
